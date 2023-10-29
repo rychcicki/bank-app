abd49938a0c7d688c3d10b6f812d5c48a8d6624d
Marcin <phppoznan@gmail.com>
2023-10-29 22:24:41 +0100

    test: added tests and comments

--- src/test/java/com/example/bank/userRegister/UserServiceTest.java
+++ src/test/java/com/example/bank/userRegister/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.bank.userRegister;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -20,145 +21,137 @@ import static org.mockito.Mockito.when;
 //@SpringBootTest - komentarz dla mnie  nie używać tej adnotacji; ładuje cały kontekst aplikacji, czyli długo trwa!!
 
 /**
- * Zważywszy na różne artykuły, na które trafiam, mam pytanie: który kod testować?
- * 1. Tylko metody serwisowe
- * 2. Tylko odpowiedzi z controller'a
- * 3. Metody serwisowe oraz odpowiedzi z controller'a
+ * Czy testy mają sprawdzać NULL i empty skoro jest walidacja ??
+ * I czy w ogóle jakoś obsługuje się tego NULL'a, bo jeżeli przekażemy go do metod CRUDowych,
+ * to leci NullPointerException.
+ * Jak na razie dane do testów typu EMPTY lub NULL są zakomentowane.
  */
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-//    private final User USER_WITH_ID3 = UserServiceUtils.userWithId3Builder();
-//    private final User USER_TO_UPDATE = UserServiceUtils.userToUpdateBuilder();
-
     @Mock
     private UserRepository userRepository;
     @InjectMocks
     private UserService userService;
 
+    @DisplayName("A parameterized test of registerUser() method")
     @ParameterizedTest
-    @MethodSource("userOver18YOSource")
-    void shouldReturnUserWhenUserIs18OrOlder(User user, UserRequest userRequest) {
-        //given
-        //when
+    @MethodSource("userOver18YearsOldSource")
+    void shouldReturnUserWhenUserIsBetween18And119YearsOld(User user, UserRequest userRequest) {
         when(userRepository.save(user)).thenReturn(user);
         User resultUser = userService.registerUser(userRequest);
-        //then
         Assertions.assertEquals(resultUser, user);
     }
 
+    @DisplayName("A parameterized test of registerUser() method")
     @ParameterizedTest
-    @MethodSource("userLessThan18YearsOldAndNullAndEmptySource")
-    void shouldReturnIllegalArgumentExceptionWhenAgeIsBelow18(UserRequest userRequest) {
-        //given
-        //when
-        //then
-        /** Czy może być tylko asercja w poprawnym teście??*/
+    @MethodSource("userRequestBelow18YearsOldAndNullAndEmptySource")
+    void shouldReturnIllegalArgumentExceptionWhenAgeIsBetween18And119(UserRequest userRequest) {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             userService.registerUser(userRequest);
         }, "User has to be adult.");
     }
 
+    @DisplayName("A parameterized test of getUser() method")
     @ParameterizedTest
-    @MethodSource("userOver18YOSource")
+    @MethodSource("userOver18YearsOldSource")
     void shouldGetUserAndReturnUserFromId3(User user) throws UserNotFoundException {
-        //given - arrange
         Long id = user.getId();
-        //when - act
-        when(userRepository.findById(id)).thenReturn(Optional.of(user));  //stub
-        User resultUser = userService.getUser(id);//act
-        //then - assert
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+        User resultUser = userService.getUser(id);
         Assertions.assertEquals(resultUser, user);
     }
 
+    @DisplayName("A parameterized test of getUser() method")
     @ParameterizedTest
     @MethodSource("userSource")
-    void shouldReturnUserExceptionWhenThereIsNoUserWithId(User user) throws UserNotFoundException {
-        //given - arrange
+    void shouldReturnUserExceptionWhenThereIsNoUserWithId(User user) {
         Long id = user.getId();
-        //when - act
-        /** W sumie to co to za test skoro dowolny user zwraca null, niezależnie od tego,
-         * czy znajduje się w db czy nie... */
         when(userRepository.findById(id)).thenReturn(Optional.empty());
-        //then - assert
         Assertions.assertThrows(UserNotFoundException.class, () -> {
             userService.getUser(id);
         });
     }
 
+    @DisplayName("A parameterized test of updateUser() method")
     @ParameterizedTest
-    @MethodSource("userOver18YOSource")
-    void shouldReturnUpdatedUser(User user, UserRequest userRequest) throws UserNotFoundException {
-        /** Nie działa dobrze - do poprawy!*/
-        //given - arrange
+    @MethodSource("userOver18YearsOldUpdateSource")
+    void shouldReturnUpdatedUser(User user, UserRequest userRequest, User updatedUser) throws UserNotFoundException {
         Long id = user.getId();
-        //when - act
         when(userRepository.findById(id)).thenReturn(Optional.of(user));
-//        when(userRepository.save(user));
-        user.setFirstName(userRequest.getFirstName());
-        user.setLastName(userRequest.getLastName());
-        user.setBirthDate(userRequest.getBirthDate());
-
         when(userRepository.save(user)).thenReturn(user);
-
         User resultUser = userService.updateUser(userRequest, user.getId());
-        user.setLastName("AABBCCCDD");
-        //then - assert
-        Assertions.assertEquals(resultUser, user);
-//        System.out.println(user);
-//        System.out.println(userRequest);
-//        verify(userRepository).save(user);
-//        verify(userRepository).findById(id);
-
-//        User resultUser = userRepository.save(USER_TO_UPDATE);
-//        User resultUser = userService.updateUser(userRequest, user.getId());
-//        verify(userService).updateUser(userRequest,user.getId());
-
+        Assertions.assertEquals(updatedUser, resultUser);
     }
 
+    @DisplayName("A parameterized test of updateUser() method")
+    @ParameterizedTest
+    @MethodSource("userOver18YearsOldSource")
+    void shouldThrowUserNotFoundExceptionDuringUserUpdateWhenThereIsNoUserWithId(User user, UserRequest userRequest) {
+        Long id = user.getId();
+        when(userRepository.findById(id)).thenReturn(Optional.empty());
+        Assertions.assertThrows(UserNotFoundException.class, () ->
+                userService.updateUser(userRequest, id));
+    }
+
+    @DisplayName("A parameterized test of updateUser() method")
+    @ParameterizedTest
+    @MethodSource("userBelow18YearsOldSource")
+    void shouldThrowIllegalArgumentExceptionDuringUserUpdateWhenTheUserIsNotAdult(User user, UserRequest userRequest) {
+        Long id = user.getId();
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                userService.updateUser(userRequest, id));
+    }
+
+    @DisplayName("A parameterized test of deleteUser() method")
     @ParameterizedTest
     @MethodSource("userSource")
     void shouldDeleteUserWhenGivenId(User user) throws UserNotFoundException {
-        /** Kompletnie nie wiem, jak przetestować metodę z serwisu.
-         * Jak zabezpieczyć metodę, żeby nie usuwała usera, jeżeli jego ID to null.*/
-        //given
+        /** Czy na tym etapie można jakoś łatwo zabezpieczyć metodę, żeby nie usuwała usera, jeżeli jego ID to null?? */
         Long id = user.getId();
-        //when
         when(userRepository.findById(id)).thenReturn(Optional.of(user));
         boolean checkUser = userRepository.findById(id).isPresent();
         userService.deleteUser(id);
         User resultUser = userRepository.findById(id).get();
-        //then
         Assertions.assertTrue(checkUser);
         verify(userRepository).delete(resultUser);
     }
 
+    @DisplayName("A parameterized test of deleteUser() method")
     @ParameterizedTest
     @MethodSource("userSource")
-    void shouldThrowUserNotFoundExceptionWhenThereIsNoUserWithId(User user) throws UserNotFoundException {
-        //given
+    void shouldThrowUserNotFoundExceptionWhenThereIsNoUserWithId(User user) {
         Long id = user.getId();
-        //when
         when(userRepository.findById(id)).thenReturn(Optional.empty());
-        //then
         Assertions.assertThrows(UserNotFoundException.class, () ->
                 userService.deleteUser(id));
     }
 
-    static Stream<Arguments> userOver18YOSource() {
-        return Stream.of(Arguments.of(userAdultBuilderWithId5(), userRequestAdultBuilder()),
-                Arguments.of(user18YearsOldBuilder(), userRequest18YOBuilder()));
+    static Stream<Arguments> userBelow18YearsOldSource/*AndEmptySource*/() {
+        return Stream.of(Arguments.of(userBelow18YearsOldBuilder(), userRequestBelow18RequestBuilder())/*,
+                Arguments.of(emptyUserBuilder(), emptyUserRequestBuilder())*/);
     }
 
-    static Stream<UserRequest> userLessThan18YearsOldAndNullAndEmptySource() {
-        return Stream.of(userRequestNotAdultBuilder(),
-                /** Czy testy mają sprawdzać NULL i empty skoro jest walidacja??
-                 * I czy w ogóle jakoś obsługuje się tego NULL'a bo leci NullPointerException*/
-                userBelow18RequestBuilder()/*, emptyUserRequestBuilder(), null*/);
+    static Stream<UserRequest> userRequestBelow18YearsOldAndNullAndEmptySource() {
+        return Stream.of(userRequestBelow18RequestBuilder()
+                /*, emptyUserRequestBuilder(), null*/);
     }
 
     static Stream<User> userSource() {
-        return Stream.of(userBuilder(), userWithId3Builder(), userAdultBuilderWithId5(), user18YearsOldBuilder(),
-                userNotAdultBuilder()/*, emptyUserBuilder(), null*/);
+        return Stream.of(userBuilder(), userWithId3Builder(), userAdultWithId5Builder(), user18YearsOldBuilder(),
+                userBelow18YearsOldBuilder()/*, emptyUserBuilder(), null*/);
     }
 
+    static Stream<Arguments> userOver18YearsOldUpdateSource() {
+        return Stream.of(Arguments.of(userAdultWithId5Builder(), updateUserRequestAdultBuilder(),
+                        resultUpdateUserWithId5Builder()),
+                Arguments.of(user18YearsOldBuilder(), updateUserRequest18YOBuilder(),
+                        resultUpdateUser18YearsOldBuilder()));
+    }
+
+    static Stream<Arguments> userOver18YearsOldSource() {
+        return Stream.of(Arguments.of(userAdultWithId5Builder(), userRequest18YOForRegisterBuilder(),
+                resultOfRegisterUserAdultWithIdBuilder())/*,
+                Arguments.of(user18YearsOldBuilder(), updateUserRequest18YOBuilder(), resultUpdateUser18YearsOldBuilder())*/);
+    }
 }
