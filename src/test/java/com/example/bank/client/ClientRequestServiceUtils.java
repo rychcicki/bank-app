da73fb9c774345be52e460fedc192b6cfb9abbbb
rychcicki <phppoznan@gmail.com>
2024-11-25 23:54:59 +0100

    tests: unit tests for ClientService

--- src/test/java/com/example/bank/client/ClientRequestServiceUtils.java
+++ src/test/java/com/example/bank/client/ClientRequestServiceUtils.java
@@ -1,35 +1,30 @@
 package com.example.bank.client;
 
-import com.example.bank.client.jpa.Address;
+import com.example.bank.client.model.Address;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-class ClientRequestServiceUtils {
-    static ClientRequest clientRequestBelow18RequestBuilder() {
-        return new ClientRequest("Zdzislaw", "Krecina",
-                LocalDate.of(2015, 4, 28), "zdzislaw.krecina@gmail.com",
-                new Address("Tatrzanska", "7B", "12-456", "Zywiec"), "password",
-                Role.ADMIN);
-    }
-
-    static ClientRequest updateClientRequestAdultBuilder() {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClientRequestServiceUtils {
+    static ClientRequest clientRequestBuilder() {
         return new ClientRequest("Czeslawa", "Cieslak",
                 LocalDate.of(1938, 6, 10), "czeslawa.cieslak@gmail.com",
                 new Address("Obroncow Warszawy", "31", "57-343", "Lewin Klodzki"),
-                "password", Role.ADMIN);
+                "password");
     }
 
-    static ClientRequest updateClientRequest18YOBuilder() {
+    static ClientRequest clientRequestExact18YearsOldBuilder() {
         return new ClientRequest("Michal", "Listkiewicz",
                 LocalDate.now().minusYears(18), "michal.listkiewicz@gmail.com",
                 new Address("Ku Ujsciu", "1", "67-890", "Gdynia"),
-                "password", Role.ADMIN);
+                "password");
     }
 
-    static ClientRequest clientRequest18YOForRegisterBuilder() {
-        return new ClientRequest("Adam", "Mialczynski",
-                LocalDate.of(1956, 11, 11), "adam.mialczynski@gmail.com",
-                new Address("Mickiewicza", "13A/3", "00-914", "Warszawa"),
-                "password", Role.ADMIN);
+    public static ClientRequest clientRequestBelow18YearsOldBuilder() {
+        return new ClientRequest("Zdzislaw", "Krecina",
+                LocalDate.now().minusYears(17), "zdzislaw.krecina@gmail.com",
+                new Address("Tatrzanska", "7B", "12-456", "Zywiec"), "password");
     }
 }
--- src/test/java/com/example/bank/client/ClientServiceUtils.java
+++ src/test/java/com/example/bank/client/ClientServiceUtils.java
@@ -1,70 +1,62 @@
 package com.example.bank.client;
 
-import com.example.bank.bank.transfer.account.Account;
-import com.example.bank.client.jpa.Address;
-import com.example.bank.client.jpa.Client;
-import com.example.bank.security.token.Token;
+import com.example.bank.client.model.Address;
+import com.example.bank.client.model.Client;
+import com.example.bank.client.model.Role;
+import com.example.bank.client.model.Status;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.Collections;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Set;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 class ClientServiceUtils {
-    static final List<Account> tempListOfAccounts = Collections.emptyList();
-    static final List<Token> tempListOfTokens = Collections.emptyList();
-
-    static Client clientBuilder() {
+    static Client clientAdultBuilder() {
         return new Client("Zdzislaw", "Krecina", LocalDate.of(1954, 4, 28),
                 "zdzislaw.krecina@gmail.com", "password",
-                new Address("Tatrzanska", "7B", "12-456", "Zywiec")
-        );
+                new Address("Tatrzanska", "7B", "12-456", "Zywiec"));
     }
 
-    static Client clientWithId3Builder() {
-        return new Client(3L, "Zdzislaw", "Krecina", LocalDate.of(1954, 4, 28),
-                "zdzislaw.krecina@gmail.com", "password", Role.ADMIN, tempListOfTokens,
-                new Address("Tatrzanska", "7B", "12-456", "Zywiec"),
-                tempListOfAccounts);
-    }
-
-    static Client clientAdultWithId5Builder() {
-        return new Client(5L, "Adam", "Mialczynski", LocalDate.of(1956, 11, 11),
-                "adam.mialczynski@gmail.com", "password", Role.ADMIN, tempListOfTokens,
-                new Address("Mickiewicza", "13A/3", "00-914", "Warszawa"),
-                tempListOfAccounts);
-    }
-
-    static Client client18YearsOldBuilder() {
-        return new Client(null, "Adam", "Malysz", LocalDate.now().minusYears(18),
-                "adam.malusz@gmail.com", "password", Role.ADMIN, tempListOfTokens,
-                new Address("Polanska", "102", "33-450", "Ustron"),
-                tempListOfAccounts);
+    static Client clientExact18YearsOldBuilder() {
+        return new Client("Adam", "Malysz", LocalDate.now().minusYears(18),
+                "adam.malusz@gmail.com", "password",
+                new Address("Polanska", "102", "33-450", "Ustron"));
     }
 
     static Client clientBelow18YearsOldBuilder() {
-        return new Client(null, "Zdzislaw", "Krecina", LocalDate.of(2015, 4, 28),
-                "zdzislaw.krecina@gmail.com", "password", Role.ADMIN, tempListOfTokens,
-                new Address("Tatrzanska", "7B", "12-456", "Zywiec"),
-                tempListOfAccounts);
+        return new Client("Zdzislaw", "Krecina", LocalDate.now().minusYears(17),
+                "zdzislaw.krecina@gmail.com", "password",
+                new Address("Tatrzanska", "7B", "12-456", "Zywiec"));
     }
 
-    static Client resultUpdateClientWithId5Builder() {
-        return new Client(5L, "Czeslawa", "Cieslak", LocalDate.of(1938, 6, 10),
-                "czeslawa.cieslak@gmail.com", "password", Role.ADMIN, tempListOfTokens,
-                new Address("Obroncow Warszawy", "31", "57-343",
-                        "Lewin Klodzki"), tempListOfAccounts);
-    }
-
-    static Client resultUpdateClient18YearsOldBuilder() {
-        return new Client(null, "Michal", "Listkiewicz", LocalDate.now().minusYears(18),
-                "michal.listkiewicz@gmail.com", "password", Role.ADMIN, tempListOfTokens,
-                new Address("Ku Ujsciu", "1", "67-890", "Gdynia"), tempListOfAccounts);
-    }
-
-    static Client resultOfRegisterClientAdultWithIdBuilder() {
-        return new Client(5L, "Adam", "Mialczynski", LocalDate.of(1956, 11, 11),
-                "adam.mialczynski@gmail.com", "password", Role.ADMIN, tempListOfTokens,
+    static ClientDTO clientDtoWithId5Builder() {
+        return new ClientDTO(5L, "Adam", "Mialczynski",
+                LocalDate.of(1956, 11, 11), "adam.mialczynski@gmail.com",
                 new Address("Mickiewicza", "13A/3", "00-914", "Warszawa"),
-                tempListOfAccounts);
+                Role.ADMIN, Status.ACTIVE, LocalDateTime.now().toString(), 1L);
+    }
+
+    static ClientDTO updatedClientDtoWithId5Builder() {
+        return new ClientDTO(5L, "Czeslawa", "Cieslak",
+                LocalDate.of(1938, 6, 10), "czeslawa.cieslak@gmail.com",
+                new Address("Obroncow Warszawy", "31", "57-343", "Lewin Klodzki"),
+                Role.USER, Status.ACTIVE, LocalDateTime.now().toString(), 1L);
+    }
+
+    static ClientDTO updatedClientExact18YearsOldBuilder() {
+        return new ClientDTO(32L, "Michal", "Listkiewicz",
+                LocalDate.now().minusYears(18), "michal.listkiewicz@gmail.com",
+                new Address("Ku Ujsciu", "1", "67-890", "Gdynia"), Role.USER,
+                Status.ACTIVE, LocalDateTime.now().toString(), 2L);
+    }
+
+    static Set<Client> listOfClients() {
+        return Set.of(clientAdultBuilder(), clientExact18YearsOldBuilder());
+    }
+
+    static Set<ClientDTO> listOfClientDTO() {
+        return Set.of(clientDtoWithId5Builder(), updatedClientExact18YearsOldBuilder());
     }
 }
