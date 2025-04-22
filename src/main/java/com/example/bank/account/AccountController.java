0fb278bb82abaccf09bbb8a0cdbff2927b44b032
rychcicki <phppoznan@gmail.com>
2025-04-22 23:27:45 +0200

    refactor: add clientId validation

--- src/main/java/com/example/bank/account/AccountController.java
+++ src/main/java/com/example/bank/account/AccountController.java
@@ -1,6 +1,7 @@
 package com.example.bank.account;
 
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -16,17 +17,17 @@ class AccountController {
 
     @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
     @PostMapping("/my-bank/{clientId}")
-    AccountDTO createMyBankAccount(@PathVariable Long clientId) {
+    AccountDTO createMyBankAccount(@PathVariable @Min(1) Long clientId) {
         return accountService.createMyBankAccount(clientId);
     }
 
     @PostMapping("/polish/{clientId}")
-    AccountDTO createPolishAccounts(@PathVariable Long clientId) {
+    AccountDTO createPolishAccounts(@PathVariable @Min(1) Long clientId) {
         return accountService.createPolishAccount(clientId);
     }
 
     @PostMapping("/foreign/{clientId}")
-    AccountDTO createForeignAccount(@PathVariable Long clientId) {
+    AccountDTO createForeignAccount(@PathVariable @Min(1) Long clientId) {
         return accountService.createForeignAccount(clientId);
     }
 
