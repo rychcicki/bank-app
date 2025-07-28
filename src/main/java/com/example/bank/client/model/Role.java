1ec5978946b29488abaea3298c71496ca0fc5b9e
rychcicki <phppoznan@gmail.com>
2025-07-28 19:49:13 +0200

    feat: add MANAGER role and update schema.sql and data.sql

--- src/main/java/com/example/bank/client/model/Role.java
+++ src/main/java/com/example/bank/client/model/Role.java
@@ -4,7 +4,8 @@ import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
     USER,
-    ADMIN;
+    ADMIN,
+    MANAGER;
 
     @Override
     public String getAuthority() {
